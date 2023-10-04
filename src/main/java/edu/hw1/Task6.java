556f75f040a30eab87f0e8d377fa2c7e61e30998
Dmitriy Mamykin <mamykindk@gmail.com>
2023-10-05 00:37:07 +0400

    Правки стиля кода

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -3,9 +3,9 @@ package edu.hw1;
 import java.util.Arrays;
 
 public class Task6 {
-    private final String KAPREKAR_CONST = "6174";
-    private final int LOWEST_BOUNDARY = 1000;
-    private final int MAX_DIGITS_COUNT = 4;
+    private final static String KAPREKAR_CONST = "6174";
+    private final static int LOWEST_BOUNDARY = 1000;
+    private final static int MAX_DIGITS_COUNT = 4;
     private final int number;
     private String stringNumber;
     private int iterationsCount;
@@ -20,7 +20,7 @@ public class Task6 {
         if (number > LOWEST_BOUNDARY
             && number <= Integer.MAX_VALUE
             && !allDigitsAreSimilar()) {
-            return Kaprekar(stringNumber);
+            return kaprekar(stringNumber);
         } else {
             return -1;
         }
@@ -37,11 +37,11 @@ public class Task6 {
         return true;
     }
 
-    private int Kaprekar(String number) {
+    private int kaprekar(String number) {
         String difference = getDifference(number);
         if (!difference.equals(KAPREKAR_CONST)) {
             iterationsCount++;
-            Kaprekar(difference);
+            kaprekar(difference);
         }
         return iterationsCount;
     }
